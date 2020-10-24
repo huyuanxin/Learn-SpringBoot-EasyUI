a0bf4b21befa43c7f3c145eedf04718b1105569d
huyuanxin <huyuanxin1999@outlook.com>
2020-10-24 18:30:51 +0800

    fix some bugs and add comments

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -18,52 +18,66 @@ public class UserController {
         this.userService = userService;
     }
 
+    // 获取全部Users
     @RequestMapping(value = "/getAllUser", method = RequestMethod.GET)
     public List<User> findAll() {
         return userService.findAll();
     }
 
+    // 通过Uid获取单个User
     @RequestMapping(value = "/getUserByUid", method = RequestMethod.GET)
     public User getOneById(
             @RequestParam("id") int id) {
         return userService.getOneByUid(id);
     }
 
+    // 通过Uid获取User.UserName
     @RequestMapping(value = "/getUserNameByUid", method = RequestMethod.GET)
     public String getUserName(
             @RequestParam("id") int uid) {
         return userService.getUserNameByUid(uid);
     }
 
+    // 通过Uid删除单个User
     @RequestMapping(value = "/deleteUserByUid", method = RequestMethod.DELETE)
     public int delete(
             @RequestParam("id") int uid) {
         return userService.deleteOneByUid(uid);
     }
 
+    // 通过RequestParam获得参数更新User
     @RequestMapping(value = "/updateUserByRequestParam", method = RequestMethod.PUT)
-    public int changeName(
-            @RequestParam("username") String userName,
+    public int updateUserByRequestParam(
+            @RequestParam(value = "username", required = false) String userName,
+            @RequestParam(value = "password", required = false) String password,
             @RequestParam("id") int uid) {
         User user = new User();
-        user.setUserName(userName);
+        if (!userName.equals("")) {
+            user.setUserName(userName);
+        }
+        if (!password.equals("")) {
+            user.setPassword(password);
+        }
         user.setUid(uid);
         return userService.updateUserByUid(user);
     }
 
+    // 通过RequestBody获得参数更新User
     @RequestMapping(value = "/updateUserByRequestBody", method = RequestMethod.PUT)
     public int updateUserByRequestBody(
-            @RequestBody User user){
+            @RequestBody User user) {
         return userService.updateUserByUid(user);
     }
 
+    // 通过RequestBody添加User
     @RequestMapping(value = "/addNewUserByRequestBody", method = RequestMethod.POST)
     public int addNewUserByRequestBody(
             @RequestBody User user
-    ){
+    ) {
         return userService.insertNewUser(user);
     }
 
+    // 通过RequestParam添加User
     @RequestMapping(value = "/addNewUserByRequestParam", method = RequestMethod.POST)
     public int addNewUserByRequestParam(
             @RequestParam(value = "username") String userName,
@@ -75,21 +89,24 @@ public class UserController {
         return userService.insertNewUser(user);
     }
 
+    // 添加多个Users
     @RequestMapping(value = "/insetMultiUser", method = RequestMethod.POST)
     public int insetMultiUser(
             @RequestBody List<User> list) {
         return userService.insertMultiUsers(list);
     }
 
+    // 通过删除Uid删除多个Users
     @RequestMapping(value = "/deleteMultiUserByUid", method = RequestMethod.DELETE)
     public int deleteMultiUserByUid(
             @RequestBody List<Integer> list) {
         return userService.deleteMultiUsersByUid(list);
     }
 
-    @RequestMapping(value = "/updateMultiUser",method = RequestMethod.PUT)
+    // 更新多个Users
+    @RequestMapping(value = "/updateMultiUser", method = RequestMethod.PUT)
     public int updateMultiUser(
-            @RequestBody List<User> list){
+            @RequestBody List<User> list) {
         return userService.updateMultiUser(list);
     }
 }
