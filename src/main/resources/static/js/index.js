e65e8507c7ef5349995d37e255b671ee53aa64e5
huyuanxin <huyuanxin1999@outlook.com>
2020-11-04 11:21:31 +0800

    完成删除业务

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,3 +1,5 @@
+
+
 function loadData() {
     $('#dg').datagrid({
         url:"/EasyUI/getUser",
@@ -5,7 +7,6 @@ function loadData() {
         pageSize:10,
         pageNumber:1,
         pageList:[10,20,50],
-        page:20,
         columns: [
             [
                 {
@@ -32,11 +33,33 @@ function loadData() {
                     formatter: function (value, row, index) {
                         return row.Password
                     }
+                },
+                {
+                    title: "操作",
+                    field:'_operate',
+                    formatter:function (value, row, index) {
+                        const rowIndex = index + 1;
+                        return '<Button onclick="deleteUser('+rowIndex+')">删除</Button>'
+                    }
                 }
             ]
         ],
         pagination: true,
     });
+}
+
+function deleteUser(index){
+    var row = $('#dg').datagrid('getData').rows[index-1];
+    $.messager.confirm('删除', '选中的信息是否删除?', function(r) {
+        if(r){
+
+            $.ajax({
+                type:"Delete",
+                url:"/User/deleteUserByUid?id="+row.Uid,
+            })
+        }
+        $('#dg').datagrid('reload');
+    })
 }
 
 onload = loadData
